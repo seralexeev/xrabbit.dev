a65300e11c5c41374afe5c9d5e02181ca00032fd
Sergey <ser.alexeev@gmail.com>
2025-08-16 23:10:15 +1000

    wip

--- transcode.sh
+++ transcode.sh
@@ -11,11 +11,11 @@ NAME="$1"
 
 echo "Transcoding $NAME"
 
-ffmpeg -hide_banner -i "static/media/$NAME.MOV" \
+ffmpeg -hide_banner -i "static/media/$NAME" \
  -c:v libx264 -profile:v high -level 4.1 -pix_fmt yuv420p \
  -preset veryfast -crf 20 \
  -c:a aac -b:a 192k \
  -movflags +faststart \
  "static/media/$NAME.mp4"
 
- rm "static/media/$NAME.MOV"
+ rm "static/media/$NAME"
